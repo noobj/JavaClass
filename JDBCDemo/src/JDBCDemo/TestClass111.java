818bb792c49ca5ce105469f43ddc892d16b9601d
300368850 <300368850@N6107S23.ad.douglascollege.ca>
2023-12-01 11:15:47 -0800

    this change is from leodev2175

---
+++ JDBCDemo/src/JDBCDemo/TestClass111.java
@@ -0,0 +1,11 @@
+package JDBCDemo;
+
+public class TestClass111 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		
+	}
+
+}
